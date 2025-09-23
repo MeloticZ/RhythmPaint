ae8c018fb5e2f10b9554e5672a5b5553b002f7d3
Korgo <hello@korgo.net>
2025-09-23 16:45:33 -0700

    Initial Commit

---
+++ Rhythm Paint/Rhythm_PaintApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rhythm_PaintApp.swift
+//  Rhythm Paint
+//
+//  Created by Korgo on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Rhythm_PaintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
